b598bcc614e44d7c48a4c616db0996298d78f8e5
ArkaMandal2003 <arkamandalofficial@gmail.com>
2024-11-03 10:00:03 +0530

    wrote code

---
+++ missingNumber.py
@@ -0,0 +1,12 @@
+n = int(input("hi:"))
+l = list()
+for i in range(1,n):
+    l = l + [int(input("enter in sorted order:"))]
+
+j = 0
+n = len(l)
+while j < n-1:
+    if l[j] != j+1:
+        print(j+1)
+        break
+    j = j + 1
